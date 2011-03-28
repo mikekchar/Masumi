c986f8f492cbe8b99f751b3f4322667043f52a4c
Mike Charlton <mikekchar@gmail.com>
2011-03-28 09:58:17 +0900

    Add Zoom and UnZoom to Swing version Ctl-Z zooms, Ctl-Shift-Z unzooms.

--- projects/masumi-swing/src/masumi/swing/widgets/Problem.java
+++ projects/masumi-swing/src/masumi/swing/widgets/Problem.java
@@ -1,13 +1,51 @@
 package masumi.swing.widgets;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
 
 import masumi.contexts.Widget;
 
 public class Problem extends JScrollPane implements Widget {
 
 	private static final long serialVersionUID = -2256119425016378573L;
+
+	private class ZoomAction extends AbstractAction {
+		private static final long serialVersionUID = -1536912129826641287L;
+		Problem problem;
+		
+		public ZoomAction(Problem aProblem) {
+			super();
+			problem = aProblem;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			problem.zoom();
+		}
+	}
+
+	private class UnZoomAction extends AbstractAction {
+
+		private static final long serialVersionUID = -1765540335925609751L;
+		Problem problem;
+		
+		public UnZoomAction(Problem aProblem) {
+			super();
+			problem = aProblem;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			problem.unzoom();
+		}
+	}
 
 	private JTextArea text;
 	
@@ -16,11 +54,53 @@ public class Problem extends JScrollPane implements Widget {
 		text = new JTextArea();
 		text.setEditable(true);
 		text.setLineWrap(true);
+		Font font = new Font("sans", Font.PLAIN, 12);
+		text.setFont(font);
 		setViewportView(text);
 		setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
 		setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_AS_NEEDED);
+	
+		setUpInputMap();
 	}
 	
+	public void setUpInputMap() {
+		ZoomAction zoom;
+		UnZoomAction unzoom;
+
+		zoom = new ZoomAction(this);
+		unzoom = new UnZoomAction(this);
+		text.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, 
+								InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK, true), 
+								"UnZoom");
+		text.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, 
+								InputEvent.CTRL_MASK, true), 
+								"Zoom");
+		text.getActionMap().put("Zoom", zoom);
+		text.getActionMap().put("UnZoom", unzoom);		
+	}
+	
+	public void zoom() {
+		Font font = text.getFont();
+		int size = font.getSize();
+		size = size + 2;
+		String name = font.getName();
+		int style = font.getStyle();
+		font = new Font(name, style, size);
+		text.setFont(font);
+	}
+	
+	public void unzoom() {
+		Font font = text.getFont();
+		int size = font.getSize();
+		if (size > 6) {
+			size = size - 2;
+			String name = font.getName();
+			int style = font.getStyle();
+			font = new Font(name, style, size);
+			text.setFont(font);
+		}
+	}
+
 	public String getText() {
 		return text.getText();
 	}
